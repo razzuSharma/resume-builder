477baacaa49bc20bf4d8a1bf0f8bea8c13b79409
razzu236 <raju.sharma.dahal@techxillence.com>
2023-08-27 22:34:30 +0545

    setting up navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h2>Hello world</h2>
+    <main className="flex min-h-screen flex-col items-center justify-center mt-9">
+      <h2>This is a home page bro wtf</h2>
     </main>
-  )
+  );
 }
