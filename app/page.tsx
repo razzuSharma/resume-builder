0ae6e83e99021adf4cc2c819dd850c26cdccb874
razzuSharma <razzuSharma008@gmail.com>
2023-11-08 18:48:58 +0545

    adding navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image'
+import Navbar from './navbar/page'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h2>Hello world</h2>
-    </main>
+      <Navbar/>
   )
 }
